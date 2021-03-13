c51773a9a31e8f7443a83090d9dd8e200e0dc7f4
Michal <michaldzurilla1@gmail.com>
2021-03-13 16:29:30 +0100

    Navbar - styling and translation added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+jest.mock("react-i18next", () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: any) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
+
 test("renders App successfully", () => {
   const { queryByTestId } = render(<App />);
   expect(queryByTestId("app")).toBeTruthy();
