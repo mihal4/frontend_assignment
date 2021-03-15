b86c146164ac8f07c33aca225832fee7ae40cdb1
Michal <michaldzurilla1@gmail.com>
2021-03-15 13:57:49 +0100

    ESLint errors fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,8 +6,10 @@ jest.mock("react-i18next", () => ({
   // this mock makes sure any components using the translate hook can use it without a warning being shown
   useTranslation: () => {
     return {
+      // eslint-disable-next-line
       t: (str: any) => str,
       i18n: {
+        // eslint-disable-next-line
         changeLanguage: () => new Promise(() => {}),
       },
     };
