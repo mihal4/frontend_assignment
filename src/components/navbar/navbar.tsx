2f8a6d07f3cf7192900d74172e9e77d772d8c1dd
Michal <michaldzurilla1@gmail.com>
2021-03-17 08:22:15 +0100

    Navbar imports fix

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Container, Title } from "../footer/footer-styled-components";
 import FbLogo from ".././assets/fb.svg";
 import InstaLogo from ".././assets/insta.svg";
-import { CenteredView, Icon, IconContainer } from "./navbar-styled-components";
+import {
+  CenteredView,
+  Icon,
+  IconContainer,
+  Container,
+  Title,
+} from "./navbar-styled-components";
 
 const Navbar = (): JSX.Element => {
   const { t } = useTranslation();
